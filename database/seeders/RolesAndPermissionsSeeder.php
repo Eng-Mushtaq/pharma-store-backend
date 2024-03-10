fd913d44daa82fbd5b00f3a9636d09599e1136bd
Eng <mushtaqhejaira@gmail.com>
2024-03-10 04:22:13 +0300

    edites

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\PermissionRegistrar;
-
 class RolesAndPermissionsSeeder extends Seeder
 {
 
